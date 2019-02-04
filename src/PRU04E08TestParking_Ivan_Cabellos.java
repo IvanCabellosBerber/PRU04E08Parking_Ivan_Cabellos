67d1826b98e0515e35156b87de74364dc6c5b916
Ivan Cabellos <icabellos2@nigul.cide.es>
2019-02-04 19:03:37 +0100

    Hacer en casa

---
+++ src/PRU04E08TestParking_Ivan_Cabellos.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PRU04E08TestParking_Ivan_Cabellos {
+
+    public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in).useDelimiter("\\n");
+        System.out.println("BIENVENIDOS A PALMA'S PARKING");
+        System.out.println("Dime que es lo que quieres hacer");
+        System.out.println("| [1] Mirar cuantas plazas de parking quedan libres |");
+        System.out.println("| [2] Mirar ");
+
+    }
+
+}
