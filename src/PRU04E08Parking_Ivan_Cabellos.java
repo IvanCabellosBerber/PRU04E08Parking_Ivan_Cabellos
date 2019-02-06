79a08b6d734805d3abb646ecd915536e22f874d3
Ivan Cabellos <icabellos2@nigul.cide.es>
2019-02-06 14:47:50 +0100

    Seguir en clase

--- src/PRU04E08Parking_Ivan_Cabellos.java
+++ src/PRU04E08Parking_Ivan_Cabellos.java
@@ -1,11 +1,10 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class PRU04E08Parking_Ivan_Cabellos {
 
-    Scanner sc = new Scanner(System.in).useDelimiter("\\n");
+    private Scanner sc = new Scanner(System.in).useDelimiter("\\n");
 
     /**
      *
@@ -30,6 +29,7 @@ public class PRU04E08Parking_Ivan_Cabellos {
     private String matricula;
     private byte posicionCoche;
     private boolean parkingMinusvalido = false;
+    private byte contadorCochesEntrados;
 
     //CONSTRUCTORES
     public PRU04E08Parking_Ivan_Cabellos(File ficheroMatriculas){
@@ -42,6 +42,7 @@ public class PRU04E08Parking_Ivan_Cabellos {
 
     public PRU04E08Parking_Ivan_Cabellos(String matriculaNueva){
         setMatricula(matriculaNueva);
+        generarEntradaCoche();
     }
 
     //GETTERS AND SETTERS
@@ -67,21 +68,15 @@ public class PRU04E08Parking_Ivan_Cabellos {
 
     public void setMatricula(String matricula) {
 
-        matricula = matricula.toUpperCase();
+        try {
+            if (isValid(matricula)){
+                this.matricula = matricula;
+            } else throw new Exception();
 
-        if (matricula.length() == 7){
-
-            for (int i = 0; i < matricula.length(); i++) {
-                if (i >= 4){
-                    if (matricula.charAt(i) < 65 && matricula.charAt(i) > 90){
-                        System.out.println("La matricula no es la correcta");
-                    }
-                }
-            }
-
-        } else {
-            System.out.print("La matricula no está bien estructurada, " +
-                    "ten en cuenta la estructura de esta [0-9]{4}[A-Z]{3}");
+        } catch (Exception e){
+            System.out.println("La matricula no es valida, introduce otra matricula");
+            matricula = sc.next();
+            setMatricula(matricula);
         }
 
     }
@@ -104,17 +99,32 @@ public class PRU04E08Parking_Ivan_Cabellos {
     }
 
 
-    //METODOS
+    //METODOS:
+
+    public void generarEntradaCoche(){
+        byte randNum = (byte) ((Math.random() * 100) + 1);
+
+        if (randNum >= 2){
+            entraCoche();
+            contadorCochesEntrados++;
+        } else if (contadorCochesEntrados == 99){
+            entraCocheMinusvalidos(); contadorCochesEntrados = 0;
+        } else {
+            entraCocheMinusvalidos(); contadorCochesEntrados = 0;
+        }
+        
+    }
 
     /* Funcion que se basa cuando entra un coche dentro del parking y aparca en un sitio normal */
     public void entraCoche(){
+
         System.out.println("El coche con matricula " + this.matricula + " ha entrado en el parking");
 
         for (int i = 0; i < lugaresReservados.length; i++) {
             if (lugaresReservados[i] == null){
                 lugaresReservados[i] = this.matricula;
 
-                setPosicionCoche((byte)i);
+                setPosicionCoche((byte) (i + 1));
                 cochesDentroDelParking.put(this.matricula, this.posicionCoche);
                 break;
             }
@@ -130,7 +140,7 @@ public class PRU04E08Parking_Ivan_Cabellos {
             if (lugaresReservadosMinusvalidos[i] == null){
                 lugaresReservadosMinusvalidos[i] = this.matricula;
 
-                setPosicionCoche((byte)i);
+                setPosicionCoche((byte) (i + 1));
                 parkingMinusvalido = true;
                 cochesDentroDelParking.put(this.matricula, this.posicionCoche);
                 break;
@@ -138,13 +148,30 @@ public class PRU04E08Parking_Ivan_Cabellos {
         }
     }
 
+    private boolean isValid(String matricula) {
+
+        matricula = matricula.toUpperCase();
+
+        if (matricula.length() > 7){
+            return false;
+        }
+
+        for (int i = 0; i < matricula.length(); i++) {
+            if (i <= 3){
+                if (!(matricula.charAt(i) >= 48 && matricula.charAt(i) <= 57)){
+                    return false;
+                }
+            } else {
+                if (!(matricula.charAt(i) >= 65 && matricula.charAt(i) <= 90)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     /* MAIN DE PRUEBAS */
     public static void main(String[] args) {
-
-        PRU04E08Parking_Ivan_Cabellos cocheMercedes = new PRU04E08Parking_Ivan_Cabellos("472267BUG");
-        cocheMercedes.entraCoche();
-        PRU04E08Parking_Ivan_Cabellos cocheRenauld = new PRU04E08Parking_Ivan_Cabellos("459822DSA");
-        cocheRenauld.entraCocheMinusvalidos();
 
         System.out.println(cochesDentroDelParking);
 
--- src/PRU04E08TestParking_Ivan_Cabellos.java
+++ src/PRU04E08TestParking_Ivan_Cabellos.java
@@ -1,18 +1,31 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-
 public class PRU04E08TestParking_Ivan_Cabellos {
 
     public static void main(String[] args) {
 
 
 
-        Scanner sc = new Scanner(System.in).useDelimiter("\\n");
+        /* Scanner sc = new Scanner(System.in).useDelimiter("\\n");
         System.out.println("BIENVENIDOS A PALMA'S PARKING");
         System.out.println("Dime que es lo que quieres hacer");
         System.out.println("| [1] Mirar cuantas plazas de parking quedan libres |");
-        System.out.println("| [2] Mirar ");
+        System.out.println("| [2] Mirar "); */
+
+        String matriculaMala = "A78552Q";
+        String matriculaBuena = "1234ABC";
+
+        PRU04E08Parking_Ivan_Cabellos coche = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche1 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche2 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche3 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche4 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche5 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche6 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche7 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche8 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+        PRU04E08Parking_Ivan_Cabellos coche9 = new PRU04E08Parking_Ivan_Cabellos(matriculaBuena);
+
+        System.out.println(coche + " " + coche1 + " " + coche2 + " " + coche3 + " " + coche4 + " " + coche5 + " " +
+                "" + coche6 + " " + coche7 + " " + coche8 + " " + coche9);
 
     }
 
